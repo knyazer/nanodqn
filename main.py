ca85a2a29f7df03e9e2ceeec59b52fec502a0b51
Roman Knyazhitskiy <git@knyaz.tech>
2025-05-15 12:03:47 +0200

    blah blah

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from tqdm.auto import tqdm
 
 from replay_buffer import ReplayBuffer
 
+args = None  # placeholder
+
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
@@ -72,9 +74,107 @@ def create_config_dataclass(cfg: DictConfig) -> Any:
     return make_dataclass("Config", fields)()
 
 
+def update(q_network, q_state, data):
+    q_next_target = q_network.apply(
+        q_state.target_params, data.next_observations
+    )  # (batch_size, num_actions)
+    q_next_target = jnp.max(q_next_target, axis=-1)  # (batch_size,)
+    next_q_value = data.rewards + (1 - data.dones) * args.gamma * q_next_target
+
+    def mse_loss(params):
+        q_pred = q_network.apply(params, data.observations)  # (batch_size, num_actions)
+        q_pred = q_pred[
+            jnp.arange(args.batch_size), data.actions.squeeze()
+        ]  # (batch_size,)
+        return ((q_pred - next_q_value) ** 2).mean(), q_pred
+
+    (loss_value, q_pred), grads = jax.value_and_grad(mse_loss, has_aux=True)(
+        q_state.params
+    )
+    q_state = q_state.apply_gradients(grads=grads)
+    return q_state, loss_value
+
+
+class DQN:
+    def __init__(self, key, envs, obs):
+        self.q_network = QNetwork(action_dim=envs.single_action_space.n)
+        self._last_mean_rs = 0
+        self.q_network.apply = jax.jit(self.q_network.apply)
+        self.q_state = TrainState.create(
+            apply_fn=self.q_network.apply,
+            params=self.q_network.init(key, obs),
+            target_params=self.q_network.init(key, obs),
+            tx=optax.adamw(learning_rate=args.learning_rate),
+        )
+
+        self.rb = ReplayBuffer(
+            buffer_size=args.buffer_size,
+            observation_space=envs.single_observation_space,
+            action_space=envs.single_action_space,
+        )
+
+    def step(self, global_step):
+        if global_step % args.train_frequency == 0:
+            data = self.rb.sample(args.batch_size)
+            self.q_state, loss = update(self.q_network, self.q_state, data)
+
+        if global_step % args.target_network_frequency == 0:
+            self.q_state = self.q_state.replace(
+                target_params=optax.incremental_update(
+                    self.q_state.params, self.q_state.target_params, args.tau
+                )
+            )
+
+    def action(self, global_step, envs, obs):
+        epsilon = linear_schedule(
+            args.start_e,
+            args.end_e,
+            args.exploration_fraction * args.total_timesteps,
+            global_step,
+        )
+        if random.random() < epsilon:
+            actions = jnp.array(
+                [envs.single_action_space.sample() for _ in range(envs.num_envs)]
+            )
+        else:
+            q_values = self.q_network.apply(self.q_state.params, obs)
+            actions = q_values.argmax(axis=-1)
+        return actions
+
+    def report(self, global_step, infos, *, wandb=None, tqdm_bar=None):
+        rs, ls = [], []
+        if "final_info" in infos:
+            for info in infos["final_info"]:
+                if info and "episode" in info:
+                    rs.append(info["episode"]["r"])
+                    ls.append(info["episode"]["l"])
+
+            current_mean = jnp.array(rs).mean()
+            if self._last_mean_rs == 0:
+                self._last_mean_rs = current_mean
+            self._last_mean_rs = current_mean * 0.05 + 0.95 * self._last_mean_rs
+
+            if args.track:  # post updates into wandb
+                if wandb is None:
+                    raise ValueError(
+                        "Trying to track, while haven't passed wandb= kwarg to report!"
+                    )
+                wandb.log(
+                    {
+                        "charts/episodic_return": jnp.array(rs).mean(),
+                        "charts/episodic_length": jnp.array(ls).mean(),
+                    },
+                    commit=False,
+                )
+
+        if global_step % 200 == 0 and tqdm_bar is not None:
+            tqdm_bar.set_description_str(f"Reward: {float(self._last_mean_rs):.2f}")
+
+
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def main(cfg: DictConfig) -> None:
     # Convert Hydra config to dynamically created dataclass
+    global args
     args = create_config_dataclass(cfg)
 
     run_name = f"{args.env_id}_{hex(int(time.time()) % 65536)}"
@@ -103,160 +203,30 @@ def main(cfg: DictConfig) -> None:
     assert isinstance(
         envs.single_action_space, gym.spaces.Discrete
     ), "only discrete action space is supported"
-
     obs, _ = envs.reset(seed=args.seed)
-    q_network = QNetwork(action_dim=envs.single_action_space.n)
-    q_network.apply = jax.jit(q_network.apply)
-    q_state = TrainState.create(
-        apply_fn=q_network.apply,
-        params=q_network.init(q_key, obs),
-        target_params=q_network.init(q_key, obs),
-        tx=optax.adamw(learning_rate=args.learning_rate),  # optimizer of choice: adamw
-    )
 
-    rb = ReplayBuffer(
-        buffer_size=args.buffer_size,
-        observation_space=envs.single_observation_space,
-        action_space=envs.single_action_space,
-    )
-
-    @jax.jit
-    def update(q_state, observations, actions, next_observations, rewards, dones):
-        q_next_target = q_network.apply(
-            q_state.target_params, next_observations
-        )  # (batch_size, num_actions)
-        q_next_target = jnp.max(q_next_target, axis=-1)  # (batch_size,)
-        next_q_value = rewards + (1 - dones) * args.gamma * q_next_target
-
-        def mse_loss(params):
-            q_pred = q_network.apply(params, observations)  # (batch_size, num_actions)
-            q_pred = q_pred[
-                jnp.arange(args.batch_size), actions.squeeze()
-            ]  # (batch_size,)
-            return ((q_pred - next_q_value) ** 2).mean(), q_pred
-
-        (loss_value, q_pred), grads = jax.value_and_grad(mse_loss, has_aux=True)(
-            q_state.params
-        )
-        q_state = q_state.apply_gradients(grads=grads)
-        return loss_value, q_pred, q_state
-
-    last_mean_rs = 0  # the average reward, reporting purposes
-
-    # TRY NOT TO MODIFY: start the game
-    obs, _ = envs.reset(seed=args.seed)
+    dqn = DQN(jr.key(args.seed + 1234), envs, obs)
 
     for global_step in (progress_bar := tqdm(range(args.total_timesteps))):
         wandb.log({})  # commit to wandb
-        # ALGO LOGIC: put action logic here
-        epsilon = linear_schedule(
-            args.start_e,
-            args.end_e,
-            args.exploration_fraction * args.total_timesteps,
-            global_step,
-        )
-        if random.random() < epsilon:
-            actions = np.array(
-                [envs.single_action_space.sample() for _ in range(envs.num_envs)]
-            )
-        else:
-            q_values = q_network.apply(q_state.params, obs)
-            actions = q_values.argmax(axis=-1)
-            actions = jax.device_get(actions)
 
-        # TRY NOT TO MODIFY: execute the game and log data.
+        actions = np.array(dqn.action(global_step, envs, obs))
         next_obs, rewards, terminations, truncations, infos = envs.step(actions)
 
-        # record rewards for reporting purposes
-        rs, ls = [], []
-        if "final_info" in infos:
-            for info in infos["final_info"]:
-                if info and "episode" in info:
-                    rs.append(info["episode"]["r"])
-                    ls.append(info["episode"]["l"])
-
-            current_mean = jnp.array(rs).mean()
-            if last_mean_rs == 0:
-                last_mean_rs = current_mean
-            last_mean_rs = current_mean * 0.05 + 0.95 * last_mean_rs
-
-            if args.track:  # post updates into wandb
-                wandb.log(
-                    {
-                        "charts/episodic_return": jnp.array(rs).mean(),
-                        "charts/episodic_length": jnp.array(ls).mean(),
-                    },
-                    commit=False,
-                )
-
-        if global_step % 200 == 0:  # print pretty updates into tty
-            progress_bar.set_description_str(f"Reward: {float(last_mean_rs):.2f}")
-
-        # save data to reply buffer; handle `final_observation`
         real_next_obs = next_obs.copy()
         for idx, trunc in enumerate(truncations):
             if trunc:
                 real_next_obs[idx] = infos["final_observation"][idx]
-        rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
+        dqn.rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
 
-        # CRUCIAL step easy to overlook, moving to the new observations
+        dqn.report(global_step, infos, wandb=wandb, tqdm_bar=progress_bar)
+
         obs = next_obs
-
-        # training process
         if global_step > args.learning_starts:
-            if global_step % args.train_frequency == 0:
-                data = rb.sample(args.batch_size)
-                # perform a gradient-descent step
-                loss, old_val, q_state = update(
-                    q_state,
-                    data.observations,
-                    data.actions,
-                    data.next_observations,
-                    data.rewards.flatten(),
-                    data.dones.flatten(),
-                )
+            dqn.step(global_step)
 
-                if global_step % 1000 == 0:  # more reporting
-                    progress_bar.set_postfix(
-                        loss=f"{float(jax.device_get(loss).squeeze()):.3f}",
-                        epsilon=f"{epsilon:.2f}",
-                    )
-
-                    if args.track:
-                        wandb.log(
-                            {
-                                "losses/td_loss": jax.device_get(loss),
-                                "losses/q_values": jax.device_get(old_val).mean(),
-                            },
-                            commit=False,
-                        )
-
-            # update target network
-            if global_step % args.target_network_frequency == 0:
-                q_state = q_state.replace(
-                    target_params=optax.incremental_update(
-                        q_state.params, q_state.target_params, args.tau
-                    )
-                )
-
-    if args.save_model:  # even more reporting
-        model_path = f"runs/{run_name}/{args.exp_name}.nanodqn_model"
-        with open(model_path, "wb") as f:
-            f.write(flax.serialization.to_bytes(q_state.params))
-        progress_bar.write(f"Model saved to {model_path}")
-
-        from cleanrl_utils.evals.dqn_jax_eval import evaluate
-
-        episodic_returns = evaluate(
-            model_path,
-            make_env,
-            args.env_id,
-            eval_episodes=10,
-            run_name=f"{run_name}-eval",
-            Model=QNetwork,
-            epsilon=0.05,
-        )
-        wandb.log({"eval/episodic_return_mean": episodic_returns.mean()})
+    if args.save_model:
+        raise NotImplementedError()
 
     envs.close()
 
